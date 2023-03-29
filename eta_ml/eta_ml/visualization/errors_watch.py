f2c1aa5c5a39e7465cc9e28c5c8a266933f7d2a4
Federico Amato <federico.amato1998@gmail.com>
2023-03-29 11:09:05 +0200

    Update environment.yml

--- eta_ml/eta_ml/visualization/errors_watch.py
+++ eta_ml/eta_ml/visualization/errors_watch.py
@@ -14,7 +14,7 @@ ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 
 PLOTS_DIR = ROOT_DIR / 'visualization/errors/violins'
 
-PLOTS_FONT_SIZE = 15
+PLOTS_FONT_SIZE = 18
 
 
 def ask_for_run_id():
@@ -40,12 +40,12 @@ def read_kts(run_id):
 
 def make_violins(kts, suptitle=None):
     data_to_plot = kts.copy()
-    fig, ax = plt.subplots(figsize=(12, 4.5))
+    fig, ax = plt.subplots(figsize=(12, 6))
     if suptitle is not None:
         fig.suptitle(suptitle, fontsize=PLOTS_FONT_SIZE)
     sns.violinplot(data_to_plot, ax=ax)
     
-    ax.set_ylim(-1, 2)
+    ax.set_ylim(-1.5, 3.1)
     ax.tick_params(axis='x', rotation=45)
     ax.axhline(y=0, ls='--', c='k')
     ax.set_ylabel("Relative error [mm/day]", fontsize=PLOTS_FONT_SIZE*1.2)
@@ -57,15 +57,15 @@ def make_violins(kts, suptitle=None):
     return ax
 
 
-def save_violins(ax, sub_dir=None):
+def save_violins(ax,  suffix='', sub_dir=None):
     saving_path = PLOTS_DIR / sub_dir if sub_dir is not None else PLOTS_DIR
     fig = ax.figure
     # PNG format
-    fig.savefig(saving_path / f'violins_{run_id}.png', dpi=300) 
+    fig.savefig(saving_path / f'violins_{suffix}_{run_id}.png', dpi=300)
     # PDF format
-    fig.savefig(saving_path / f'violins_{run_id}.pdf')
+    fig.savefig(saving_path / f'violins_{suffix}_{run_id}.pdf')
     # EPS format
-    fig.savefig(saving_path / f'violins_{run_id}.eps')
+    fig.savefig(saving_path / f'violins_{suffix}_{run_id}.eps')
     return None
 
 
@@ -80,9 +80,9 @@ def main(run_id=None):
         models_data.columns = [regressor.upper() + model_name.strip('model ') 
                                for model_name in models_data.columns]
         ax = make_violins(models_data)
-        save_violins(ax)
+        save_violins(ax, suffix=regressor)
     
-    # SELECT IRRIGATION SEASON
+    # SELECT IRRIGATION SEASON.
     # Irrigation season starts at 15 May and ends 31 Sep
     irrigation_kt = kts.loc[(kts.index.dayofyear > 167) & (kts.index.month < 10)]
     
@@ -93,10 +93,10 @@ def main(run_id=None):
         models_data.columns = [regressor.upper() + model_name.strip('model ') 
                                for model_name in models_data.columns]
         ax = make_violins(models_data)
-        save_violins(ax, sub_dir = 'irrigation_season')
+        save_violins(ax, suffix=regressor, sub_dir='irrigation_season')
 
 
 if __name__ == "__main__":
     # Use the log ID of the run you want to analyze
-    run_id = '71200X'
+    run_id = '55323T'
     main(run_id)
